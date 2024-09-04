af6d310711bf3926abfece058c7454e7378e7203
Lindsay Morales <lindsaymorales15@gmail.com>
2024-09-04 16:21:49 -0600

    create candidate registration page

---
+++ ui/src/pages/candidate/register.page.tsx
@@ -0,0 +1,16 @@
+
+import Head from 'next/head';
+import styles from '../styles/RegisterCandidate.module.css';
+
+export default function RegisterCandidate() {
+
+  return (
+    <>
+      <Head>
+        <title>Candidate Registration</title>
+        <meta name="description" content="Register candidates for the current election." />
+        <link rel="icon" href="/assets/favicon.ico" />
+      </Head>
+    </>
+  );
+}
