4a4cef943217df1591d9515e9d705e30e247eda1
Lindsay Morales <lindsaymorales15@gmail.com>
2024-09-07 11:09:09 -0600

    add zkapp workers

---
+++ ui/src/pages/zkAppWorker.ts
@@ -0,0 +1,93 @@
+// Based on mina/o1labs docs: https://github.com/o1-labs/docs2/blob/main/examples/zkapps/04-zkapp-browser-ui/ui/src/pages/zkappWorker.ts
+// Adapted to work with HandleVotes contract
+
+import { Mina, PublicKey, fetchAccount } from 'o1js';
+
+type Transaction = Awaited<ReturnType<typeof Mina.transaction>>;
+
+// ---------------------------------------------------------------------------------------
+
+import type { Votes } from '../../../contracts/src/HandleVotes';
+
+const state = {
+  Votes: null as null | typeof Votes,
+  zkapp: null as null | Votes,
+  transaction: null as null | Transaction,
+};
+
+// ---------------------------------------------------------------------------------------
+
+const functions = {
+  setActiveInstanceToDevnet: async (args: {}) => {
+    const Network = Mina.Network(
+      'https://api.minascan.io/node/devnet/v1/graphql'
+    );
+    console.log('Devnet network instance configured.');
+    Mina.setActiveInstance(Network);
+  },
+  loadContract: async (args: {}) => {
+    const { Votes } = await import('.././../../contracts/src/HandleVotes');
+    state.Votes = Votes;
+  },
+  compileContract: async (args: {}) => {
+    await state.Votes!.compile();
+  },
+  fetchAccount: async (args: { publicKey58: string }) => {
+    const publicKey = PublicKey.fromBase58(args.publicKey58);
+    return await fetchAccount({ publicKey });
+  },
+  initZkappInstance: async (args: { publicKey58: string }) => {
+    const publicKey = PublicKey.fromBase58(args.publicKey58);
+    state.zkapp = new state.Votes!(publicKey);
+  },
+  // todo: add functions to interact with the contract
+
+  // getNum: async (args: {}) => {
+  //   const currentNum = await state.zkapp!.num.get();
+  //   return JSON.stringify(currentNum.toJSON());
+  // },
+  // createUpdateTransaction: async (args: {}) => {
+  //   const transaction = await Mina.transaction(async () => {
+  //     await state.zkapp!.update();
+  //   });
+  //   state.transaction = transaction;
+  // },
+  proveUpdateTransaction: async (args: {}) => {
+    await state.transaction!.prove();
+  },
+  getTransactionJSON: async (args: {}) => {
+    return state.transaction!.toJSON();
+  },
+};
+
+// ---------------------------------------------------------------------------------------
+
+export type WorkerFunctions = keyof typeof functions;
+
+export type ZkappWorkerRequest = {
+  id: number;
+  fn: WorkerFunctions;
+  args: any;
+};
+
+export type ZkappWorkerReponse = {
+  id: number;
+  data: any;
+};
+
+if (typeof window !== 'undefined') {
+  addEventListener(
+    'message',
+    async (event: MessageEvent<ZkappWorkerRequest>) => {
+      const returnData = await functions[event.data.fn](event.data.args);
+
+      const message: ZkappWorkerReponse = {
+        id: event.data.id,
+        data: returnData,
+      };
+      postMessage(message);
+    }
+  );
+}
+
+console.log('Web Worker Successfully Initialized.');
---
+++ ui/src/pages/zkAppWorkerClient.ts
@@ -0,0 +1,100 @@
+// Based on mina/o1labs docs: https://github.com/o1-labs/docs2/blob/main/examples/zkapps/04-zkapp-browser-ui/ui/src/pages/zkappWorkerClient.ts
+// Adapted to work with HandleVotes contract
+
+import { Field, PublicKey, fetchAccount } from 'o1js';
+
+import type {
+  WorkerFunctions,
+  ZkappWorkerReponse,
+  ZkappWorkerRequest,
+} from './zkAppWorker';
+
+export default class ZkappWorkerClient {
+  // ---------------------------------------------------------------------------------------
+
+  setActiveInstanceToDevnet() {
+    return this._call('setActiveInstanceToDevnet', {});
+  }
+
+  loadContract() {
+    return this._call('loadContract', {});
+  }
+
+  compileContract() {
+    return this._call('compileContract', {});
+  }
+
+  fetchAccount({
+    publicKey,
+  }: {
+    publicKey: PublicKey;
+  }): ReturnType<typeof fetchAccount> {
+    const result = this._call('fetchAccount', {
+      publicKey58: publicKey.toBase58(),
+    });
+    return result as ReturnType<typeof fetchAccount>;
+  }
+
+  initZkappInstance(publicKey: PublicKey) {
+    return this._call('initZkappInstance', {
+      publicKey58: publicKey.toBase58(),
+    });
+  }
+
+  //todo: add functions to interact with the contract
+
+//   async getNum(): Promise<Field> {
+//     const result = await this._call('getNum', {});
+//     return Field.fromJSON(JSON.parse(result as string));
+//   }
+
+//   createUpdateTransaction() {
+//     return this._call('createUpdateTransaction', {});
+//   }
+
+  proveUpdateTransaction() {
+    return this._call('proveUpdateTransaction', {});
+  }
+
+  async getTransactionJSON() {
+    const result = await this._call('getTransactionJSON', {});
+    return result;
+  }
+
+  // ---------------------------------------------------------------------------------------
+
+  worker: Worker;
+
+  promises: {
+    [id: number]: { resolve: (res: any) => void; reject: (err: any) => void };
+  };
+
+  nextId: number;
+
+  constructor() {
+    this.worker = new Worker(new URL('./zkappWorker.ts', import.meta.url));
+    this.promises = {};
+    this.nextId = 0;
+
+    this.worker.onmessage = (event: MessageEvent<ZkappWorkerReponse>) => {
+      this.promises[event.data.id].resolve(event.data.data);
+      delete this.promises[event.data.id];
+    };
+  }
+
+  _call(fn: WorkerFunctions, args: any) {
+    return new Promise((resolve, reject) => {
+      this.promises[this.nextId] = { resolve, reject };
+
+      const message: ZkappWorkerRequest = {
+        id: this.nextId,
+        fn,
+        args,
+      };
+
+      this.worker.postMessage(message);
+
+      this.nextId++;
+    });
+  }
+}
